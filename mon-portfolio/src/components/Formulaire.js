def4a9482e073fd614e6032fe9d9a7d30595a8a0
shenzor <toppan.frederic@gmail.com>
2023-12-07 10:59:23 +0100

    formulaire ok avec formspree

--- mon-portfolio/src/components/Formulaire.js
+++ mon-portfolio/src/components/Formulaire.js
@@ -1,29 +1,44 @@
-// src/components/Contact.js
 import React from 'react';
-import '../animations/formulaire-contact.css'
+import '../animations/formulaire-contact.css';
+import { useForm, ValidationError } from '@formspree/react';
 
+const Formulaire = () => {
+  const [state, handleSubmit] = useForm('mqkvrpyp');
 
+  if (state.succeeded) {
+    return <p>Merci pour votre message!</p>;
+  }
 
-
-const Formulaire= () => {
   return (
-    <form className='back-intro' style={{marginBottom:'30px', borderRadius:'10px'}}>
+    <form
+      className="back-intro"
+      style={{ marginBottom: '30px', borderRadius: '10px' }}
+      onSubmit={handleSubmit}
+    >
       <div className="segment ">
         <h1>Contact</h1>
       </div>
 
       <label>
-        <input type="text" placeholder="Email Address" />
+        <input type="text" name="first_name" placeholder="First Name" />
       </label>
+
       <label>
-        <input type="first name" placeholder="First Name" />
+        <input type="text" name="email" placeholder="Email Address" />
       </label>
-        <label>
-          <input type="text" placeholder="Message" style={{height:"100px", width:'320px',borderRadius:'15px'}}/>
-        </label>
-     <div className="input-group">
-        </div>
-     <button className="red" type="button">
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+      <label>
+        <textarea
+          className="form-control textarea"
+          name="message"
+          placeholder="Message"
+        />
+      </label>
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+      <div className="input-group"></div>
+      <button className="red" type="submit" disabled={state.submitting}>
         <i className="icon ion-md-lock"></i> Submit
       </button>
     </form>
--- mon-portfolio/src/components/Introduction.js
+++ mon-portfolio/src/components/Introduction.js
@@ -1,60 +1,73 @@
 // src/components/Introduction.js
-import React from 'react';
-import '../style.css'
-import '../animations/animation-sphere.css';
-import OrbAnimation from './OrbAnimation';
-import portrait from'../images/20180912_110504.webp';
+import React from "react";
+import "../style.css";
+import "../animations/animation-sphere.css";
+import OrbAnimation from "./OrbAnimation";
+import portrait from "../images/20180912_110504.webp";
 
 const Introduction = () => {
   return (
-  <div className=' back-intro ' >
-
-     <span className=" circular " > 
-     <img  src={portrait} alt='portrait' className="circular-image" style={{padding:'4px'}} />
-     </span>
+    <div className=" back-intro ">
+      <span className=" circular ">
+        <img
+          src={portrait}
+          alt="portrait"
+          className="circular-image"
+          style={{ padding: "4px" }}
+        />
+      </span>
       {/* <h2 className='h-intro' style={{paddingTop:'10px'}}>Présentation </h2> */}
-   <OrbAnimation/>
-   <div>
-    <p className='letter-space intro-contact p1' style={{marginTop:'200px'}}>
-Je m'appelle <span className='h-intro' style={{border:'none', fontWeight:'bold',fontSize:'22px'}}> Frédéric TOPPAN, </span>enthousiaste du code et passionné par le monde numérique, je suis un développeur web dédié à créer des expériences en ligne exceptionnelles. 
-Avec une solide formation et une expérience pratique, je suis constamment à la recherche de nouvelles façons d'innover et d'améliorer la façon 
-dont les utilisateurs interagissent avec le web. 
-</p>
-<p className='letter-space intro-contact p2'>
-Ma boîte à outils comprend une expertise approfondie dans les langages de programmation tels que HTML, CSS, JavaScript, 
-ainsi que des frameworks modernes tels que React.js et Node.js. J'ai développé et déployé avec succès une variété de sites web réactifs, 
-du simple site vitrine aux applications web complexes.
-</p>
-<p className='letter-space intro-contact p3'>
-Mon approche du développement web se caractérise par une attention méticuleuse aux détails, 
-un engagement envers la qualité du code et un désir constant d'apprendre et de m'adapter aux dernières tendances de l'industrie. 
-Je suis convaincu que la collaboration étroite avec les clients est essentielle pour comprendre leurs besoins uniques et créer 
-des solutions sur mesure qui dépassent leurs attentes.
-</p>
-<p className='letter-space intro-contact p4'>
-Je crois fermement en la collaboration et en la communication transparente. 
-Travailler en équipe pour résoudre des problèmes complexes et atteindre des objectifs communs est quelque chose que j'apprécie particulièrement.
-Ma capacité d'adaptation me permet de rester "Agile" dans un environnement en constante évolution, 
-m'assurant ainsi de toujours rester à la pointe de la technologie.
-</p>
-<p className='letter-space intro-contact p5'>
-Si vous recherchez un développeur web passionné par son métier et engagé à offrir des solutions de qualité, 
-je serais ravi de discuter de la façon dont nous pouvons collaborer pour atteindre vos objectifs numériques.
+      <OrbAnimation />
+      <div>
+        <p
+          className="letter-space intro-contact p1"
+          style={{ marginTop: "200px" }}
+        >
+          Je m'appelle{" "}
+          <span
+            className="h-intro"
+            style={{ border: "none", fontWeight: "bold", fontSize: "22px" }}
+          >
+            {" "}
+            Frédéric TOPPAN,{" "}
+          </span>
+          enthousiaste du code et passionné par le monde numérique, je suis un
+          développeur web dédié à créer des expériences en ligne
+          exceptionnelles. Avec une solide formation et une expérience pratique,
+          je suis constamment à la recherche de nouvelles façons d'innover et
+          d'améliorer la façon dont les utilisateurs interagissent avec le web.
         </p>
-        </div>
-      {/* <h3 className='h-intro'>    Objectifs</h3> 
-      <span className='letter-space' style={{color:'white',textShadow: "0 0 5px #4fc3f7, 0 0 10px #65c8f7, 0 0 15px #4fc3f7" }}>Collaboration et Adaptabilité</span>
-      <p className='letter-space'>
-        Je crois fermement en la collaboration et en la communication transparente. 
-        Travailler en équipe pour résoudre des problèmes complexes et atteindre des objectifs communs est quelque chose que j'apprécie particulièrement.
-        Ma capacité d'adaptation me permet de rester agile dans un environnement en constante évolution, 
-        m'assurant ainsi de toujours rester à la pointe de la technologie.
-        Contribuer à des projets innovants en apportant ma passion pour le développement front-end, tout en continuant à élargir mes compétences dans cet univers dynamique.</p>
-        <h3 className='h-intro'>savoir être </h3>
-        <p className='letter-space'>Aisance à l'oral, force de proposition.Excellentes compétences d'écoute et empathie.Expérience antérieure en leadership commercial.</p> */}
-        
+        <p className="letter-space intro-contact p2">
+          Ma boîte à outils comprend une expertise approfondie dans les langages
+          de programmation tels que HTML, CSS, JavaScript, ainsi que des
+          frameworks modernes tels que React.js et Node.js. J'ai développé et
+          déployé avec succès une variété de sites web réactifs, du simple site
+          vitrine aux applications web complexes.
+        </p>
+        <p className="letter-space intro-contact p3">
+          Mon approche du développement web se caractérise par une attention
+          méticuleuse aux détails, un engagement envers la qualité du code et un
+          désir constant d'apprendre et de m'adapter aux dernières tendances de
+          l'industrie. Je suis convaincu que la collaboration étroite avec les
+          clients est essentielle pour comprendre leurs besoins uniques et créer
+          des solutions sur mesure qui dépassent leurs attentes.
+        </p>
+        <p className="letter-space intro-contact p4">
+          Je crois fermement en la collaboration et en la communication
+          transparente. Travailler en équipe pour résoudre des problèmes
+          complexes et atteindre des objectifs communs est quelque chose que
+          j'apprécie particulièrement. Ma capacité d'adaptation me permet de
+          rester "Agile" dans un environnement en constante évolution,
+          m'assurant ainsi de toujours rester à la pointe de la technologie.
+        </p>
+        <p className="letter-space intro-contact p5">
+          Si vous recherchez un développeur web passionné par son métier et
+          engagé à offrir des solutions de qualité, je serais ravi de discuter
+          de la façon dont nous pouvons collaborer pour atteindre vos objectifs
+          numériques.
+        </p>
       </div>
-      
+    </div>
   );
 };
 
