f5f84ddc667ed7633acf5577e9d6d464e98da8fa
shenzor <toppan.frederic@gmail.com>
2023-12-09 18:14:37 +0100

    logo cards 90%

--- mon-portfolio/src/pages/Projects.js
+++ mon-portfolio/src/pages/Projects.js
@@ -7,9 +7,17 @@ function Project() {
 
   return (
     <div className="back-contact">
-      <h3 className="letter-space contact-text p1" style={{border:'1px solid white', padding:'5px',textAlign:'center',borderRadius:'5px'}}>
-         J'ai eu le privilège de travailler sur une variété de
-        projets stimulants, allant de sites web réactifs à des applications
+      <h3
+        className="letter-space contact-text p1"
+        style={{
+          border: "1px solid white",
+          padding: "5px",
+          textAlign: "center",
+          borderRadius: "5px",
+        }}
+      >
+        J'ai eu le privilège de travailler sur une variété de projets
+        stimulants, allant de sites web réactifs à des applications
         interactives. Ces expériences ont renforcé ma conviction que le
         développement front-end va bien au-delà du code il s'agit de créer des
         solutions significatives qui améliorent la vie des utilisateurs.
@@ -18,21 +26,44 @@ function Project() {
         {projects.map((project) => (
           <div key={project.id} className="project-card">
             <div className="card-front">
-            {project.future && <p className="in-progress-text">In Progress</p>}
-              <img src={project.image} alt={project.title} className="card-image" />
-              <h3 className="card-title">{project.title}</h3>
+              {project.future && (
+                <p className="in-progress-text">In Progress</p>
+              )}
+              <img
+                src={project.image}
+                alt={project.title}
+                className="card-image"
+              />
+              <img
+                src={project.logo}
+                className="card-title"
+                alt={project.title}
+              />
             </div>
             <div className="card-back">
               <p className="card-description">{project.description}</p>
               <ul className="card-technologies">
                 {project.technologies.map((tech) => (
                   <li key={tech.name}>
-                    <img src={tech.icon} alt={tech.name} className="tech-icon" />
+                    <img
+                      src={tech.icon}
+                      alt={tech.name}
+                      className="tech-icon"
+                    />
                   </li>
                 ))}
               </ul>
-              <a href={project.github} target="_blank" rel="noopener noreferrer">
-                <img src="/images/2447911.webp" alt="GitHub" className="github-icon" style={{width:'50px',height:'50px',paddingTop:'30px'}}/>
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="/images/2447911.webp"
+                  alt="GitHub"
+                  className="github-icon"
+                  style={{ width: "50px", height: "50px", paddingTop: "30px" }}
+                />
               </a>
             </div>
           </div>
