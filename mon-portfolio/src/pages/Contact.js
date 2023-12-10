77b46ea91d27a059fbe2958c1ad02967e7696857
shenzor <toppan.frederic@gmail.com>
2023-12-10 01:29:28 +0100

    projects ok

--- mon-portfolio/src/pages/Contact.js
+++ mon-portfolio/src/pages/Contact.js
@@ -6,7 +6,7 @@ import Formulaire from '../components/Formulaire';
 function Contact() {
     return (
       <div className=' back-contact ' >
-<h3 className='h-intro intro-contact' >Si vous recherchez un développeur front-end passionné, engagé à offrir des expériences exceptionnelles, n'hésitez pas à me contacter.
+<h3 className='h-intro intro-contact' style={{textAlign:'center'}} >Si vous recherchez un développeur front-end passionné, engagé à offrir des expériences exceptionnelles, n'hésitez pas à me contacter.
   J'adorerais discuter de la manière dont je peux contribuer à la réussite de votre prochain projet.</h3>
        <Formulaire/> 
        
