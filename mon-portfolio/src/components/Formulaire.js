565c3f88c437be4b90d5fa0dca21f3a6c6df4cec
shenzor <toppan.frederic@gmail.com>
2023-12-14 22:40:29 +0100

    ajout email "required" au Form

--- mon-portfolio/src/components/Formulaire.js
+++ mon-portfolio/src/components/Formulaire.js
@@ -2,27 +2,40 @@ import React from "react";
 import "../animations/formulaire-contact.css";
 import { useForm, ValidationError } from "@formspree/react";
 
+// ... (importations existantes)
+
 const Formulaire = () => {
   const [state, handleSubmit] = useForm("mqkvrpyp");
 
   if (state.succeeded) {
-    return <p>Merci pour votre message!</p>;
+    return <p style={{color:'black', fontWeight:'900', fontFamily:'Poppins', fontSize:'32px'}}>Merci pour votre message!</p>;
   }
 
   return (
     <form
       className="back-contact"
-      style={{ marginBottom: "30px", borderRadius: "10px",  }}
+      style={{ marginBottom: "30px", borderRadius: "10px" }}
       onSubmit={handleSubmit}
     >
       <div className="segment ">
         <h1>Contact</h1>
       </div>
       <label>
-        <input type="text" name="first_name" placeholder="First Name" />
+        <input
+          type="text"
+          name="first_name"
+          placeholder="First Name"
+          required // Champ obligatoire
+        />
       </label>
       <label>
-        <input type="text" name="email" placeholder="Email Address" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          required // Champ obligatoire
+          aria-invalid={state.errors && state.errors.email ? "true" : "false"}
+        />
       </label>
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <label>
@@ -32,8 +45,15 @@ const Formulaire = () => {
           placeholder="Message"
         />
       </label>
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+      />
       <div className="input-group"></div>
+      {state.errors && state.errors.email && (
+        <p style={{ color: "red" }}>Veuillez saisir une adresse e-mail valide.</p>
+      )}
       <button className="red" type="submit" disabled={state.submitting}>
         <i className="icon ion-md-lock"></i> Submit
       </button>
